b1b6de713cf2bc199397b8cd8c91654ecee879a5
arsalxan <thearsalansiddiqui@gmail.com>
2025-02-10 11:47:13 +0530

    first commit

---
+++ src/personal_finance_dashboard/Personal_finance_dashboard.java
@@ -0,0 +1,12 @@
+
+package personal_finance_dashboard;
+
+
+public class Personal_finance_dashboard {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
